cd5102f921e4e0366c9c96b0e4321f4d5482063b
FinnT730 <finnt730@gmail.com>
2019-10-17 00:05:55 +0200

    Almost 3D translations!!
    Signed-off-by: FinnT730 <finnt730@gmail.com>

--- src/cam/Main.java
+++ src/cam/Main.java
@@ -15,17 +15,17 @@ public class Main extends Component {
                 {0,0,1,0,1*z[0]},
                 {0,0,0,1}
         };
-        x[0] = x[0] * matrix[0][0];
-        x[1] = x[1] * matrix[0][1];
-        x[1] = x[1] * matrix[0][2];
+        x[0] = x[0] + matrix[0][0];
+        x[1] = x[1] + matrix[0][1];
+        x[1] = x[1] + matrix[0][2];
 
-        y[0] = y[0] * matrix[1][0];
-        y[1] = y[1] * matrix[1][1];
-        y[1] = y[1] * matrix[1][2];
+        y[0] = y[0] + matrix[1][0];
+        y[1] = y[1] + matrix[1][1];
+        y[1] = y[1] + matrix[1][2];
 
-        z[0] = z[0] * matrix[2][0];
-        z[1] = z[1] * matrix[2][1];
-        z[1] = z[1] * matrix[2][2];
+        z[0] = z[0] + matrix[2][0];
+        z[1] = z[1] + matrix[2][1];
+        z[1] = z[1] + matrix[2][2];
 
         res[0] = x;
         res[1] = y;
@@ -55,26 +55,27 @@ public class Main extends Component {
 
     }
 
+    Thread thread = new Thread();
 
     @Override
     public void paint(final Graphics g) {
         super.paint(g);
         Utils util = new Utils();
-        assert util != null;
-        for(int p = 0; p < 360; p++) {
-            float[] i = new float[]{p + 50, 30, 16};
-            float[] j = new float[]{75, 50, p * 7};
-            float[] k = new float[]{2 * 40, p + 5, p * p};
-            float[][] x = mat3D(i, j, k);
-            g.setColor(Color.RED);
+
+            for (int p = 0; p < 360; p++) {
+                float[] i = new float[]{p + 50, 30, 16};
+                float[] j = new float[]{75, 50, p * 7};
+                float[] k = new float[]{2 * 40, p + 5, p * p};
+                float[][] x = mat3D(i, j, k);
+                g.setColor(Color.RED);
 //            g.drawLine(frame.getWidth() / 2, frame.getHeight() / 2 + 50, (int) x[0][1], (int) util.Vec3Mul(i,j,k)[0]);
-            g.drawLine(frame.getWidth() / 2 - (int)x[0][1],frame.getHeight() / 2 - (int)x[0][1],(int)util.Vec3Add(i,j,k)[0],(int)util.Vec3Add(i,j,k)[1] / 5);
-            g.setColor(Color.BLACK);
+                g.drawLine(frame.getWidth() / 2 - (int) x[0][1], frame.getHeight() / 2 - (int) x[0][1], (int) util.Vec3Add(i, j, k)[0], (int) util.Vec3Add(i, j, k)[1] / 5);
+                g.setColor(Color.BLACK);
 //            g.drawLine(frame.getWidth() / 2, frame.getHeight() / 2 + 50, (int) x[0][0], (int) util.Vec3Mul(i,j,k)[1]);
-            g.drawLine(frame.getWidth() / 2 - (int)x[0][1],frame.getHeight() / 2 - (int)x[1][0],(int)util.Vec3Add(i,j,k)[0],(int)util.Vec3Add(i,j,k)[1]);
-            g.setColor(Color.BLUE);
+                g.drawLine(frame.getWidth() / 2 - (int) x[0][1], frame.getHeight() / 2 - (int) x[1][0], (int) util.Vec3Add(i, j, k)[0], (int) util.Vec3Add(i, j, k)[1]);
+                g.setColor(Color.BLUE);
 //            g.drawLine(frame.getWidth() / 2, frame.getHeight() / 2 + 50, (int) x[1][1], (int) util.Vec3Mul(i,j,k)[2]);
-            g.drawLine(frame.getWidth() / 2 - (int)x[0][1],frame.getHeight() / 2 - (int)x[1][1],(int)util.Vec3Add(i,j,k)[0],(int)util.Vec3Add(i,j,k)[0]);
+                g.drawLine(frame.getWidth() / 2 - (int) x[0][1], frame.getHeight() / 2 - (int) x[1][1], (int) util.Vec3Add(i, j, k)[0], (int) util.Vec3Add(i, j, k)[0]);
+            }
         }
-    }
 }
